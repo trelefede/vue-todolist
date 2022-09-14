7050730e5f1d7b9d774a8fed1da8a306669adc1f
TreleaniFederico <treleani.federico@gmail.com>
2022-09-14 22:48:03 +0200

    add array objects add text line-trought if done is true

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const toDoList = [
+    {
+        text: 'Carosello di immagini',
+        done: true,
+    },
+    {
+        text: 'Carosello di caroselli',
+        done: false,
+    },
+    {
+        text: 'Carosello di piselli',
+        done: false,
+    },
+    {
+        text: 'Immagini di carosello',
+        done: true,
+    },
+    {
+        text: 'Correzione carosello',
+        done: true,
+    }
+]
+
+console.log(toDoList);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        toDoList,
+    }
+
+
+});
